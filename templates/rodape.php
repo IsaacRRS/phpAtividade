1a512a4e73e8a24a78f411e9b19073f33a017b42
Isaac <zackhyou@gmail.com>
2024-09-28 20:55:45 -0300

    💄 - css

--- templates/rodape.php
+++ templates/rodape.php
@@ -9,3 +9,48 @@
     </div>
 </footer>
 
+<style>
+body {
+    margin: 0;
+    padding: 0;
+}
+footer {
+    background: #15475c;
+    color: #ffffff;
+    padding: 20px 0;
+    text-align: center;
+    position: fixed;
+    bottom: 0;
+    height: 65px;
+    width:100%;
+}
+
+footer .caixa {
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 0 20px;
+    }
+
+footer ul {
+    list-style: none;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    margin-top: 10px;
+}
+
+footer ul li {
+    margin: 0 15px;
+}
+
+footer ul li a {
+    color: #ffffff;
+    text-decoration: none;
+    transition: color 0.3s;
+}
+
+footer ul li a:hover {
+    color: #1abc9c;
+}
+
+</style>
--- templates/topo.php
+++ templates/topo.php
@@ -7,3 +7,43 @@
     </ul>
 </header>
 
+
+<style>
+    header {
+    background-color: #7c7c88; 
+    padding: 20px; 
+    border-bottom: 2px solid #51575c; 
+    display: flex;
+    justify-content: space-between; 
+    align-items: center; 
+}
+
+header h2 {
+    font-size: 24px; 
+    color: white; 
+}
+
+header ul { 
+    display: flex; 
+    list-style-type: none; 
+    margin: 0; 
+    padding: 0; 
+}
+
+header ul li {
+    margin-left: 20px; 
+}
+
+header ul li a {
+    font-size: 18px; 
+    color: white; 
+    text-decoration: none; 
+    transition: color 0.3s; 
+}
+
+header ul li a:hover {
+    color: #92410b; 
+}
+
+
+</style>
