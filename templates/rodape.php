0f521e57850e8974d4a8a16cebe53cac2ae06041
Isaac <zackhyou@gmail.com>
2024-09-28 20:53:08 -0300

    🎉 - criação TEMPLATES

---
+++ templates/rodape.php
@@ -0,0 +1,11 @@
+<footer>
+    <div class="caixa">
+        <p>&copy; <?php echo date("Y"); ?> Rodapé</p>
+        <ul>
+            
+            <li><a href="#">Política de Privacidade</a></li>
+            <li><a href="#">Termos </a></li>
+        </ul>
+    </div>
+</footer>
+
---
+++ templates/topo.php
@@ -0,0 +1,9 @@
+<header>
+    <h2>Topo</h2>
+    <ul>
+        <li><a href='?pg=index'>Página Inicial</a></li>
+        <li><a href='?pg=fale'>Fale conosco</a></li>
+        <li><a href='?pg=quemsomos'>Quem Somos</a></li>
+    </ul>
+</header>
+
